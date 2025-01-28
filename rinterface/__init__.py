83a2af06a3f05536a916df268773d0ee09500129
Will Decker <will.decker@gatech.edu>
2025-01-28 10:18:17 -0500

    update __init__.py

--- rinterface/__init__.py
+++ rinterface/__init__.py
@@ -1,3 +1,14 @@
 from .rinterface import rinterface
+import subprocess
+
+def is_r_installed():
+    try:
+        result = subprocess.run(['Rscript', '--version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return result.returncode == 0
+    except FileNotFoundError:
+        return False
+    
+if not is_r_installed():
+    raise RuntimeError("Rscript is not installed on your system. Please install R to use this package.")
 
 __all__ = ["rinterface"]
