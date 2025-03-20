323ecbee556ad570be780dc132b1a96f8de8d549
Will Decker <will.decker@gatech.edu>
2025-03-20 09:40:35 -0400

    `v0.0.4`

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rinterface",
-    version="0.0.3",
+    version="0.0.4",
     author="Will Decker",
     author_email="will.decker@gatech.edu",
     description="Quickly interface with R in Python",
